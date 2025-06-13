8ad1ec444ad9188e543b7f8ad14b3555a31ae4a8
RazaqFarhanR <aljawadfarhan@gmail.com>
2025-06-14 04:10:42 +0700

    feat: implement searching on admin

--- app/admin/members/page.tsx
+++ app/admin/members/page.tsx
@@ -8,6 +8,7 @@ import React, { useEffect, useState } from 'react'
 import { toast } from 'sonner';
 
 export default function MembersPage() {
+  const [originalMembers, setOriginalMembers] = useState<Member[]>([]);
   const [members, setMembers] = useState<Member[]>([])
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedMemberStatus, setSelectedMemberStatus] = useState(false);
@@ -41,15 +42,34 @@ export default function MembersPage() {
         success: "Banned or unbanned member successfully.",
         error: 'Something went wrong!',
       });  
-      getMembers().then(setMembers)
-      // console.log(`Deleting member with ID: ${selectedMemberId}`);
+      getMembers().then((data) => {
+        setMembers(data);
+        setOriginalMembers(data);
+      });
       closeModal();
     }
   };
 
   useEffect(() => {
-    getMembers().then(setMembers)
+    getMembers().then((data) => {
+      setMembers(data);
+      setOriginalMembers(data);
+    });
+
   }, [])
+
+  const handleSearch = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const searchInput = (event.currentTarget.elements.namedItem("search") as HTMLInputElement).value.toLowerCase();
+
+    const filtered = originalMembers.filter((member) =>
+      member.name.toLowerCase().includes(searchInput) ||
+      member.email.toLowerCase().includes(searchInput)
+    );
+
+    setMembers(filtered);
+  };
+
 
   return (
     <div>
@@ -61,15 +81,21 @@ export default function MembersPage() {
       <div className='w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 mt-6'>
           {/* card header */}
           <div className='flex justify-between items-center space-x-6 w-full'>              
-            <form className="w-full">   
+            <form className="w-full" onSubmit={handleSearch}>   
                 <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
                 <div className="relative">
                     <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                         <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                         </svg>
-                    </div>
-                    <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required />
+                    </div>    
+                    <input
+                      type="search"
+                      id="search"
+                      name="search"
+                      className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="Search by name or email"
+                    />
                     <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
                 </div>
             </form>
--- components/ui-admin/transaction-tables.tsx
+++ components/ui-admin/transaction-tables.tsx
@@ -50,6 +50,7 @@ const TransactionTables: React.FC<TransactionTablesProps> = ({ transaction, open
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
+                <th scope="col" className="px-6 py-3">Invoice</th>
                 <th scope="col" className="px-6 py-3">Customer Name</th>
                 <th scope="col" className="px-6 py-3">Date</th>
                 <th scope="col" className="px-6 py-3">Total Amount</th>
@@ -64,8 +65,11 @@ const TransactionTables: React.FC<TransactionTablesProps> = ({ transaction, open
                 className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600"
                 >
                     <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        {transaction.member?.name}
+                        {transaction.invoiceId}
                     </th>
+                    <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        {transaction.member?.name}
+                    </td>
                     <td className="px-6 py-4">
                     {formatDate(transaction.trxDate)}
                     </td>
